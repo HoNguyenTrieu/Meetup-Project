25c15d0a916c93bd43a4c1b3549a4ca54bd91b02
Nguyen Trieu <nguyentrieussj4@gmail.com>
2021-05-27 23:14:24 +0700

    display comment

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -22,7 +22,9 @@ const commentController = {
       );
       await newComment.save();
       res.json({ newComment });
-    } catch (err) {}
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
   },
 };
 
