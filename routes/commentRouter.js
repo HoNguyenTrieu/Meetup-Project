2d6223ab2aedd878d4a9172245e98784d9df6475
Nguyen Trieu <nguyentrieussj4@gmail.com>
2021-05-28 00:46:23 +0700

    commit before sleep, comment feature

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -3,5 +3,6 @@ const commentController = require("../controllers/comment.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/comment", authMiddleware, commentController.createComment);
+router.patch("/comment/:id", authMiddleware, commentController.updateComment);
 
 module.exports = router;
