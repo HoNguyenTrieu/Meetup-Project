c3cc2566f92a8236043370286c4b2e678ff5ef2a
Nguyen Trieu <nguyentrieussj4@gmail.com>
2021-05-31 23:26:15 +0700

    moving on message

--- routes/notifyRouter.js
+++ routes/notifyRouter.js
@@ -6,4 +6,18 @@ router.post("/notify", authMiddleware, notifyController.createNotify);
 
 router.delete("/notify/:id", authMiddleware, notifyController.removeNotify);
 
+router.get("/notifies", authMiddleware, notifyController.getNotifies);
+
+router.patch(
+  "/isReadNotify/:id",
+  authMiddleware,
+  notifyController.isReadNotify
+);
+
+router.delete(
+  "/deleteAllNotify",
+  authMiddleware,
+  notifyController.deleteAllNotifies
+);
+
 module.exports = router;
